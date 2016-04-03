91c609bc25a1cf3899f24ec9785085c0da958d88
Yunus Dawji <ydawji@gmail.com>
2016-04-03 18:13:02 -0400

    clean up

--- rundeck-d42-nodes-plugin-master/script.sh
+++ rundeck-d42-nodes-plugin-master/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp ./build/libs/rundeck-ec2-nodes-plugin-master-0.1.0-SNAPSHOT.jar /Users/yunusdawji/Documents/Github/rundeck-ec2-nodes-plugin/libext/
+cp ./build/libs/rundeck-d42-nodes-plugin-master-0.1.0-SNAPSHOT.jar /Users/yunusdawji/Documents/Github/rundeck-ec2-nodes-plugin/libext/
